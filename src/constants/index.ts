7a224a3dc3d0a116dd81913d2413f5d34009edea
Wang Lei <wanglei.ok@hotmail.com>
2020-10-28 17:09:30 +0800

    fix router_address to origin

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0xcb1d0f24c3bf717d22cb33249efade0c355215f3'
+export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
